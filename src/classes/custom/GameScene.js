a6f4001ebe07836fd21c2843ba9e00cd74d64fbc
Dan Koenig <dan.koenig@helloworld.com>
2022-05-30 21:08:55 -0400

    Creating Batter and setting up game

--- src/classes/custom/GameScene.js
+++ src/classes/custom/GameScene.js
@@ -1,9 +1,19 @@
 import BaseScene from '../BaseScene';
+import GroundController from '../controllers/GroundController';
+import LightController, {LightTypes} from '../controllers/LightController';
+import BatterController from './BatterController';
 export default class GameScene extends BaseScene{
     constructor(el){
         super(el);
     }
     initialize(){
         // your code here
+        const lightController = new LightController({environment: this.environment});
+        lightController.addLight({type: LightTypes.DIRECTIONAL, color: 0xffffff, intensity: 1.5, target: {x: 20, y: 0, z: 15}});
+        lightController.addLight({type: LightTypes.DIRECTIONAL, color: 0xffffff, intensity: .5, target: {x: -5, y: 0, z: -22}});
+
+        new GroundController({environment: this.environment}, 'https://danielpatrickkoenig.github.io/spirit-of-kovak/dist/dirt_row.png');
+
+        new BatterController({environment: this.environment});
     }
 }
